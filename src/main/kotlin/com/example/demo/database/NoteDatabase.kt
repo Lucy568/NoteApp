bfa7f42a8c5987091cb48c44d2adc214649b097e
okeke chidinma <chidinma1365@gmail.com>
2020-07-02 10:17:33 +0100

    added two notes as primary

--- src/main/kotlin/com/example/demo/database/NoteDatabase.kt
+++ src/main/kotlin/com/example/demo/database/NoteDatabase.kt
@@ -5,6 +5,25 @@ import java.util.*
 
 class NoteDatabase {
     var notes = mutableListOf<Note>()
+    var note1 = Note()
+    var note2 = Note()
+
+    init {
+        note1.id = "1"
+        note1.title = "Note"
+        note1.body = "chicken"
+        note1.image = "tea"
+        note1.time = "Fri Jun 19 13:12:45 WAT 2020"
+
+        note2.id = "2"
+        note2.title = "Note2"
+        note2.body = "chicken2"
+        note2.image = "tea2"
+        note2.time = "Fri Jun 19 13:12:45 WAT 2020"
+        notes.add(note1)
+        notes.add(note2)
+    }
+
     fun notesData(): MutableList<Note>{
 
         return notes
