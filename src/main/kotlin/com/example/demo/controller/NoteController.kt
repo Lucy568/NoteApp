4c9f97eb8fb550b43e91ce05f828d1b9173e3d5b
okeke chidinma <chidinma1365@gmail.com>
2020-07-02 10:35:14 +0100

    fix call issue

--- src/main/kotlin/com/example/demo/controller/NoteController.kt
+++ src/main/kotlin/com/example/demo/controller/NoteController.kt
@@ -6,14 +6,12 @@ import com.example.demo.model.Note
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.rmi.activation.ActivationGroupID
 import java.util.*
 
 @RestController
+@CrossOrigin(origins = arrayOf("*"))
 class NoteController {
     val noteDatabase : NoteDatabase = NoteDatabase()
         @GetMapping("/listnotes")
